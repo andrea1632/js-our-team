db5026fadabb75f6eb9ff5b3553d2ce2f19b85c7
andrea1632 <andrea.bellucci10@libero.it>
2022-03-24 15:24:02 +0100

    creazione array di oggetti

---
+++ asset/js/main.js
@@ -0,0 +1,38 @@
+var team = 
+[
+    {
+        'nome' : 'Wayne Barnett',
+        'ruolo' : 'Founder & CEO',
+        'photo' : 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        'nome' : 'Angela Carroll',
+        'ruolo' : 'Chief Editor',
+        'photo' : 'angela-carroll-chief-editor.jpg'
+    },
+
+    {
+        'nome' : 'Walter Gordon',
+        'ruolo' : 'Office Manager',
+        'photo' : 'walter-gordon-office-manager.jpg'
+    },
+
+    {
+        'nome' : 'Angela Lopez',
+        'ruolo' : 'Social Media Manager',
+        'photo' : 'angela-lopez-social-media-manager.jpg'
+    },
+
+    {
+        'nome' : 'Scott Estrada',
+        'ruolo' : 'Developer',
+        'photo' : 'scott-estrada-developer.jpg'
+    },
+
+    {
+        'nome' : 'Barbara Ramos',
+        'ruolo' : 'Graphic Designer',
+        'photo' : 'barbara-ramos-graphic-designer.jpg'
+    }
+]
